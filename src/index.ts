c68e195358a88d3afb7c208d06e72e7e2e388ea7
morochobrandon <bggutierrez.21@est.ucab.edu.ve>
2024-04-17 19:45:41 -0400

    agregue express al proyecto

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("hola mundo");
+import express from 'express'
+const app = express()
+app.use(express.json()) // middleware que transforma la req.body a un json
+
+const PORT = 3000
+
+app.get('/ping',(_req,res)=>{
+  console.log('someone pinged here!!')
+  res.send('pong')
+})
+
+app.listen(PORT,()=>{
+  console.log(`Server running on port ${PORT}`);
+})
